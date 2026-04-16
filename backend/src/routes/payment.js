b84dc7e9be955b50a2a278d035bc1c451444ee24
mrvaliev2022-wq <mrvaliev2022@gmail.com>
2026-04-16 14:53:43 +0500

    Add payment.js route

---
+++ backend/src/routes/payment.js
@@ -0,0 +1,10 @@
+﻿const router = require('express').Router();
+router.get('/card-info', (req, res) => {
+  res.json({
+    number: process.env.CARD_NUMBER || '5614681264029681',
+    owner:  process.env.CARD_OWNER  || 'V***** O**** M.',
+    bank:   process.env.CARD_BANK   || 'Uzcard',
+    amount: process.env.CARD_AMOUNT || '29 000',
+  });
+});
+module.exports = router;
